a76b1add8f086a52d26b391e354e3cc76fd33b78
gonagon4 <82447045+gonagon4@users.noreply.github.com>
2021-05-16 19:24:28 +0200

    Borrar automata y declarar variables

---
+++ PRACTICA-MODELOS/src/es/ceu/gisi/modcomp/webcrawler/jflex/JFlexScraper.java
@@ -0,0 +1,47 @@
+package es.ceu.gisi.modcomp.webcrawler.jflex;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Esta clase encapsula toda la lógica de interacción con el parser HTML.
+ *
+ * @author Sergio Saugar García <sergio.saugargarcia@ceu.es>
+ */
+
+public class JFlexScraper {
+    HTMLParser analizador; 
+    private Stack PILA = new Stack();
+    private List <String> IMAGES = new ArrayList();
+    private List <String> LINKS = new ArrayList();
+    private boolean isBalanced = false;
+    private int estado = 0;
+    
+
+    public JFlexScraper(File fichero) throws FileNotFoundException {
+        Reader reader = new BufferedReader(new FileReader(fichero));
+        analizador = new HTMLParser(reader);
+    }
+
+    // Esta clase debe contener tu automata programado...
+    public ArrayList<String> obtenerHiperenlaces() {
+        // Habrá que programarlo..
+        return new ArrayList<String>();
+    }
+
+    public ArrayList<String> obtenerHiperenlacesImagenes() {
+        // Habrá que programarlo..
+        return new ArrayList<String>();
+    }
+
+    public boolean esDocumentoHTMLBienBalanceado() {
+        // Habrá que programarlo..
+        return false;
+    }
+}
